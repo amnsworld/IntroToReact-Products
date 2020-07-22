a06a3a778f9467de04df235cb641882020e8f9d1
Amandeep Singh <you@example.com>
2020-07-21 20:38:07 -0400

    completed the setup as per the notion steps

--- src/js/components/Greeting.js
+++ src/js/components/Greeting.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React from 'react';
+import test from 'img/userDemoImg.jpg';
 
 const Greeting = ({name}) => {
-	return  (
-	<h1>Hello, {name}!</h1>
-	)
+	return (
+	<>
+		<img src={test} alt="Just a test image" class="user-img"/>
+		<h1>Hello, {name}!</h1>
+	</>)
 }
 
 export default Greeting;
